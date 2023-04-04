d4c99fcd18d0e14c4f0077e9f89f24ccd0fd8079
ConnerKT <connerthompson121@gmail.com>
2023-04-04 02:08:16 -0500

    Added Comments
    Added Comments for clarification.

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-// We need to grab the form
-
+// Global Variables to contain the container (form) and the submit button (input)
 let container = document.getElementById("containerForm");
-let submitbutton = document.getElementById("todoText")
+let submitButton = document.getElementById("todoText")
 
+// Setting the state of two objects, one to an empty string and one to an empty array
 let stateArray = [];
 let submitValue = "";
 
 // A fun thing I made, the random variable will be set each time to a random prompt :)
 let valueArrayDefault = ["Lets get started!","New Idea?","Enter Your Next Plan","Write it Down!","Lets have a Good Day!","Type here...", "Lets Plan Something!","Got Any Big Plans?","What's the next big thing?","Plan it out first!","Start first, worry later!","Stroke of genius?",'Share your big idea',"What's your plan?",'Got any genius schemes?','Tell us your thoughts','Input your next plan',"Let's hear it!",'Brain blast, go!','Your turn to share','Unleash your creativity',"What's cooking, friend?"];
 
+// Get Random function so we can randomize between the length of our prompts
 function getRandom(){
     return Math.floor(Math.random() * valueArrayDefault.length);
 }
 
+//This inputs the random variable so each one is chosen
 function randomTextPrompt(){
     let randomVar = getRandom();
-submitbutton.value = valueArrayDefault[randomVar]
+submitButton.value = valueArrayDefault[randomVar]
 }
-
+// This event listener listens for a 'submit' and triggers an event, which prevents the default, sets submitvalue to the value of the button.
 container.addEventListener("submit", function(event){
     event.preventDefault();
-    submitValue = submitbutton.value
+    submitValue = submitButton.value
     randomTextPrompt();
 });
 
