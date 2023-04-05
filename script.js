f8c7e1ed96eda2a0f3778469d9508ad61d5ea6e4
ConnerKT <connerthompson121@gmail.com>
2023-04-05 15:57:47 -0500

    Fixed Button Appearance

--- script.js
+++ script.js
@@ -10,35 +10,38 @@ let stateArray = [];
 let submitValue = "";
 
 // A fun thing I made, the random variable will be set each time to a random prompt :)
-let valueArrayDefault = ["Lets get started!","New Idea?","Enter Your Next Plan","Write it Down!","Lets have a Good Day!","Type here...", "Lets Plan Something!","Got Any Big Plans?","What's the next big thing?","Plan it out first!","Start first, worry later!","Stroke of genius?",'Share your big idea',"What's your plan?",'Got any genius schemes?','Tell us your thoughts','Input your next plan',"Let's hear it!",'Brain blast, go!','Your turn to share','Unleash your creativity',"What's cooking, friend?"];
+let valueArrayDefault = ["Lets get started!", "New Idea?", "Enter Your Next Plan", "Write it Down!", "Lets have a Good Day!", "Type here...", "Lets Plan Something!", "Got Any Big Plans?", "What's the next big thing?", "Plan it out first!", "Start first, worry later!", "Stroke of genius?", 'Share your big idea', "What's your plan?", 'Got any genius schemes?', 'Tell us your thoughts', 'Input your next plan', "Let's hear it!", 'Brain blast, go!', 'Your turn to share', 'Unleash your creativity', "What's cooking, friend?"];
 
 // Get Random function so we can randomize between the length of our prompts
-function getRandom(){
+function getRandom() {
     return Math.floor(Math.random() * valueArrayDefault.length);
 }
 
 //This inputs the random variable so each one is chosen
-function randomTextPrompt(){
+function randomTextPrompt() {
     let randomVar = getRandom();
-submitButton.value = valueArrayDefault[randomVar]
+    submitButton.value = valueArrayDefault[randomVar]
 }
+let todoIndex = 0
 // This event listener listens for a 'submit' and triggers an event, which prevents the default, sets submitvalue to the value of the button.
-container.addEventListener("submit", function(event){
-    
+container.addEventListener("submit", function (event) {
+
     let inputBox = document.createElement("input");
     let labelBox = document.createElement("label");
     let removeButton = document.createElement("button");
     let todoDiv = document.createElement("div")
-
+    inputBox.index = todoIndex
+    removeButton.index = todoIndex
+    todoIndex++
     event.preventDefault();
     submitValue = submitButton.value
     // Input Validation for the submitted value
-    if (submitValue === ""){
-        alert ("Please Enter Something...")
+    if (submitValue === "") {
+        alert("Please Enter Something...")
         return
     }
-    for (i = 0; i < valueArrayDefault.length;i++){
-        if (submitValue === valueArrayDefault[i]){
+    for (i = 0; i < valueArrayDefault.length; i++) {
+        if (submitValue === valueArrayDefault[i]) {
             alert("Don't enter our prompts!");
             return
         }
@@ -47,6 +50,7 @@ container.addEventListener("submit", function(event){
     labelBox.textContent = submitValue;
     removeButton.textContent = "X";
     removeButton.style.visibility = "hidden";
+    removeButton.setAttribute("class","removeButton");
     todoList.append(todoDiv)
     todoDiv.append(inputBox)
     inputBox.type = "checkbox";
@@ -55,26 +59,33 @@ container.addEventListener("submit", function(event){
     //Resetting the Text Box
     randomTextPrompt();
 
-//     let checkbox = document.querySelector("input[type=checkbox]")
-//     checkbox.addEventListener("change", function(){
-//     if (checkbox.checked == true){
-//         removeButton.style.visibility = "visible";  
-//     }else if (checkbox.checked == false){
-//        removeButton.style.visibility = "hidden";   
-//     }
-    
-// })
+    //     let checkbox = document.querySelector("input[type=checkbox]")
+    //     checkbox.addEventListener("change", function(){
+    //     if (checkbox.checked == true){
+    //         removeButton.style.visibility = "visible";  
+    //     }else if (checkbox.checked == false){
+    //        removeButton.style.visibility = "hidden";   
+    //     }
 
-let checkbox = document.querySelector("input[type=checkbox]")
-todoList.addEventListener("change", function(event){
+    // })
 
-if (event.target.checked == true){
-    removeButton.style.visibility = "visible";  
-}else if (event.target.checked == false){
-   removeButton.style.visibility = "hidden";   
-}
 
-})
+    todoList.addEventListener("change", function (event) {
+
+        let removeButtonClass = document.getElementsByClassName("removeButton");
+        // console.log("event.target.index", event.target.index)
+        for (let i = 0; i < removeButtonClass.length; i++){
+            if( removeButtonClass[i].index === event.target.index){
+                if (removeButtonClass[i].style.visibility == "hidden") {
+                    removeButtonClass[i].style.visibility = "visible";
+                    console.log("removeButtonClass[i].index", removeButtonClass[i].index, event.target.index)
+                } else if (removeButtonClass[i].style.visibility  == "visible") {
+                    removeButtonClass[i].style.visibility = "hidden";
+                }
+            }
+        }
+      
+    })
 });
 
 
